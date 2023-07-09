a47f7868c8f0bffc080b305d55312139257a39b8
Anna Rocha <annaleletr@gmail.com>
2023-07-09 20:10:39 -0300

    interface

---
+++ src/src/components/template/Footer.jsx
@@ -0,0 +1,7 @@
+import './Footer.css'
+import React from 'react'
+
+export default props => 
+    <footer className="footer">
+        <span> Desenvolvido por <strong> AnnaTRocha</strong></span>
+    </footer>
